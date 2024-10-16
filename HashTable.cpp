fed1016ba66db17b24904863f38bf14bac1d1b27
Neha <nehapalak2684@gmail.com>
2024-10-16 15:35:26 +0530

    2nd commit

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,92 +1,147 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 class HashTable {
-private:
-    vector<int> keys;
-    vector<int> values;
-    vector<bool> occupied;
-    int capacity;
-    int numElements;
+    public:
+        double load_fact = 0.8;
+        int m;
+        int size = 0;
+        std::vector<int> table;
 
-    int hash(int key) {
-        return key % capacity;
-    }
-
-    void resize() {
-        int oldCapacity = capacity;
-        capacity *= 2; // Double the size
-        vector<int> oldKeys = keys;
-        vector<int> oldValues = values;
-        keys.resize(capacity);
-        values.resize(capacity);
-        fill(occupied.begin(), occupied.end(), false);
-        numElements = 0;
-        for (int i = 0; i < oldCapacity; i++) {
-            if (occupied[i]) {
-                insert(oldKeys[i], oldValues[i]);
-            }
+        HashTable(int initialSize) {
+            m = initialSize;
+            table.resize(m, -1); 
         }
-    }
 
-public:
-    HashTable(int initialSize) : capacity(initialSize), numElements(0) {
-        keys.resize(capacity);
-        values.resize(capacity);
-        occupied.resize(capacity, false);
-    }
-
-    void insert(int key, int value) {
-        if (float(numElements) / capacity >= 0.8) {
-            resize();
-        }
-        int index = hash(key);
-        int i = 0;
-        while (occupied[index]) {
-            index = (hash(key) + i * i) % capacity;
-            i++;
-        }
-        keys[index] = key;
-        values[index] = value;
-        occupied[index] = true;
-        numElements++;
-    }
-
-    int search(int key) {
-        int index = hash(key);
-        int i = 0;
-        while (occupied[index]) {
-            if (keys[index] == key) {
-                return values[index];
-            }
-            index = (hash(key) + i * i) % capacity;
-            i++;
-        }
-        return -1; // Key not found
-    }
-
-    void remove(int key) {
-        int index = hash(key);
-        int i = 0;
-        while (occupied[index]) {
-            if (keys[index] == key) {
-                occupied[index] = false;
-                numElements--;
+        void insert(int value){
+            checkIfResize();
+            int key = quadratic_probe(value);
+            if(key == -1){
+                cout << "Max probing limit reached!" << endl;
+                return;
+            } else if(key == -2){
+                cout << "Duplicate key insertion is not allowed" << endl;
                 return;
             }
-            index = (hash(key) + i * i) % capacity;
-            i++;
-        }
-    }
-};
 
-int main() {
-    HashTable ht(10);
-    ht.insert(1, 100);
-    ht.insert(2, 200);
-    cout << "Search for key 1: " << ht.search(1) << endl;
-    ht.remove(1);
-    cout << "Search for key 1 after deletion: " << ht.search(1) << endl;
-    return 0;
-}
+            table[key] = value;
+            size ++;
+        }
+
+        void remove(int value){
+            int key = quadratic_probe_search(value);
+            if(key == -1){
+                cout << "Element not found" << endl;
+                return;
+            }
+            table[key] = -1;
+            size --;
+        }
+
+        int search(int value){
+            return quadratic_probe_search(value);
+        }
+
+        void printTable(){
+            for(auto itr : table){
+                if(itr == -1){
+                    cout << "-" << " ";
+                }else{
+                    cout << itr << " ";
+                }
+            }
+            cout << '\n';
+            return;
+        }
+
+    private:
+        void checkIfResize(){
+            if((double)size / (double)m > load_fact) {
+                m = getNewSize();
+                std::vector<int> new_table(m, -1);
+                table.swap(new_table);
+                size = 0;
+                for(auto itr: new_table){
+                    if(itr == -1){
+                        continue;
+                    }
+                    else{
+                        insert(itr);
+                    }
+                }
+                // Deallocate memory
+                new_table = std::vector<int>();
+            }
+        }
+
+        int getNewSize(){
+            int new_size = m * 2 + 1;
+            while(!isPrime(new_size)){
+                new_size ++;
+            }
+            return new_size;
+        }
+
+        bool isPrime(int n){
+            int i;
+            double sqrt_n = sqrt(n) ;
+            for (i = 2; i <= sqrt_n ; i++)
+            {
+                if (n % i == 0){
+                    return false;
+                    break;
+                }
+            }
+            return true;
+
+        }
+
+        int hash(int value){
+            return value % m;
+        }
+        
+        int quadratic_probe(int value){
+            int max = (m + 1) / 2;
+            int key;
+            bool found = false;
+
+            for(int step=0; step <= max; step++){
+                key = (hash(value) + step*step) % m;
+                if(table[key] == -1){
+                    found = true;
+                    break;
+                } else if(table[key] == value){
+                    return -2;
+                }
+
+            }
+            
+            if(found == false){
+                return -1;
+            }
+            else{
+                return key;
+            }
+        }
+
+        int quadratic_probe_search(int value){
+            int max = (m + 1) / 2;
+            int key;
+
+            for(int step = 0; step <= max; step++){
+                key = (hash(value) + step*step) % m;
+                if(table[key] == -1){
+                    return -1;
+
+                }else if (table[key] == value){
+                    return key;
+                }
+                
+
+            }
+
+            return -1;
+        }
+};
